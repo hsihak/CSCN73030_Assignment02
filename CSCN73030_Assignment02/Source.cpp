0043a5b55c8a214d1bfb72f312ce3362cbaa989b
Hangsihak Sin <hsin9104@conestogac.on.ca>
2024-09-20 11:39:53 -0400

    Add project files.

---
+++ CSCN73030_Assignment02/Source.cpp
@@ -0,0 +1,14 @@
+/*******************************************************************
+*	Assignment 01:		Debugging vs. Release Coding Practice
+*	Course:				CSCN73030-24F
+*	Section:			2
+*	Professor:			Sara-Ross Howe
+*	Author:				Hangsihak Sin
+*	Date:				September/19/2024
+* *******************************************************************/
+
+#include <iostream>
+
+int main(void) {
+	return 1;
+}
